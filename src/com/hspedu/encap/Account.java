1ed5ea9fb579253a0c5087fe43666d75bb065e20
DrifterMo <872964584@qq.com>
2023-02-17 18:58:29 +0800

    2023年2月17日 learning

---
+++ src/com/hspedu/encap/Account.java
@@ -0,0 +1,8 @@
+package com.hspedu.encap;
+
+/**
+ * @Author
+ * @date 2023/2/17 14:34
+ */
+public class Account {
+}
---
+++ src/com/hspedu/encap/AccountTest.java
@@ -0,0 +1,21 @@
+package com.hspedu.encap;
+
+/**
+ * @Author
+ * @date 2023/2/17 14:34
+ */
+
+/**
+ * 创建程序,在其中定义两个类：Account 和 AccountTest 类体会 Java 的封装性。
+ * Account 类要求具有属性：姓名（长度为 2 位 3 位或 4 位）、余额(必须>20)、
+ * 密码（必须是六位）, 如果不满足，则给出提示信息，并给默认值(程序员自己定)
+ * 通过 setXxx 的方法给 Account 的属性赋值。
+ * 在 AccountTest 中测试
+ */
+public class AccountTest {
+    private String name;
+    private double balance;
+    private String password;
+
+
+}
