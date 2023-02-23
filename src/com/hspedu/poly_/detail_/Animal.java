4f4d457c3d2d5e41a5519b8a49678a8afd4fb568
DrifterMo <872964584@qq.com>
2023-02-23 18:59:16 +0800

    2023年2月23日 learning

---
+++ src/com/hspedu/poly_/detail_/Animal.java
@@ -0,0 +1,9 @@
+package com.hspedu.poly_.detail_;
+
+/**
+ * @Author
+ * @date 2023/2/23 18:58
+ */
+public class Animal {
+
+}
---
+++ src/com/hspedu/poly_/detail_/Cat.java
@@ -0,0 +1,8 @@
+package com.hspedu.poly_.detail_;
+
+/**
+ * @Author
+ * @date 2023/2/23 18:59
+ */
+public class Cat {
+}
