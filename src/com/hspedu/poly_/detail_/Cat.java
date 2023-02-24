90819378d790901ebca25faa5b6b3519132e9c43
DrifterMo <872964584@qq.com>
2023-02-24 12:17:29 +0800

    2023年2月24日 learning

--- src/com/hspedu/poly_/detail_/Cat.java
+++ src/com/hspedu/poly_/detail_/Cat.java
@@ -4,5 +4,13 @@ package com.hspedu.poly_.detail_;
  * @Author
  * @date 2023/2/23 18:59
  */
-public class Cat {
+public class Cat extends Animal{
+    //方法重写
+    public void eat(){
+        System.out.println("猫吃鱼");
+    }
+
+    public void catchMouse(){
+        System.out.println("猫抓老鼠");
+    }
 }
---
+++ src/com/hspedu/poly_/detail_/Dog.java
@@ -0,0 +1,9 @@
+package com.hspedu.poly_.detail_;
+
+/**
+ * @Author
+ * @date 2023/2/24 9:57
+ */
+public class Dog extends Animal{
+
+}
