218538f73564ac0724b251549de152e8184bdf1a
Ikaros <ikaros@o1olife.com>
2023-04-07 20:32:46 +0800

    4-7 泛型

---
+++ src/com/hspedu/generic/GenericDetail.java
@@ -0,0 +1,51 @@
+package com.hspedu.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @date 2023/4/7
+ */
+public class GenericDetail {
+    public static void main(String[] args) {
+        //1.给泛型指向数据类型时，要求是引用类型，不能是基本数据类型
+        List<Integer> list = new ArrayList<Integer>();
+        //List<Integer> list2 = new ArrayList<int>();   //错误
+
+        //2. 说明
+        //因为 E 指定了 A 类型，构造器传入了 new A()
+        //在给泛型指定具体类型后，可以传入该类型或者其子类类型
+        Pig<A> aPig = new Pig<A>(new A());
+        aPig.f();
+        Pig<A> aPig2 = new Pig<A>(new B());
+        aPig2.f();
+    }
+}
+
+class Tiger<E>{//类
+    E e;
+
+    public Tiger(){
+
+    }
+
+    public Tiger(E e){
+        this.e = e;
+    }
+}
+
+class A{}
+
+class B extends A{}
+
+class Pig<E>{//
+    E e;
+
+    public Pig(E e){
+        this.e = e;
+    }
+
+    public void f(){
+        System.out.println(e.getClass());   //运行类型
+    }
+}
