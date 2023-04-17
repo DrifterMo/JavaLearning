78a506ba6351607e3d0a4de2c83f0970b91bfd73
DrifterMo <ikaros@o1olife.com>
2023-04-17 18:51:34 +0800

    4-17 反射

---
+++ src/com/hspedu/reflection/Cat.java
@@ -0,0 +1,11 @@
+package com.hspedu.reflection;
+
+/**
+ * @date 2023/4/17
+ */
+public class Cat {
+    private String name = "招财猫";
+    public void hi(){
+        System.out.println("hi " + name);
+    }
+}
